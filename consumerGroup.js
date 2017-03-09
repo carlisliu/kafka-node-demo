19e27220c3f9f74219ee885272c4d32b580c1a83
Carlis Liu <carlisliu@live.com>
2017-03-09 10:47:24 +0800

    fix error

--- consumerGroup.js
+++ consumerGroup.js
@@ -4,54 +4,61 @@ consumer ID do not need to be defined. There's a new ID to represent consumers c
 var kafka = require('kafka-node');
 var ConsumerGroup = kafka.ConsumerGroup;
 var options = {
-	host: 'zookeeper:2181',
-	groupId: 'test-consumer-group',
-	protocol: ['roundrobin'],
-	migrateHLC: false,
-	migrateRolling: true
+    host: '10.194.1.2:2181',
+    groupId: 'test-consumer-group',
+    protocol: ['roundrobin'],
+    /*protocol: [{
+        name: '',
+        version: '',
+        userData: {}
+        assign: function(topicPartition, groupMembers, callback) {
+            
+        }
+    }],*/
+    migrateHLC: false,
+    migrateRolling: true
 };
 var topicPartition = {
-	"RebalanceTopic": [
-		"0",
-		"1",
-		"2"
-	],
-	"RebalanceTest": [
-		"0",
-		"1",
-		"2"
-	]
+    "RebalanceTopic": [
+        "0",
+        "1",
+        "2"
+    ],
+    "RebalanceTest": [
+        "0",
+        "1",
+        "2"
+    ]
 };
 
 var groupMembers = [{
-		"subscription": [
-			"RebalanceTopic",
-			"RebalanceTest"
-		],
-		"version": 0,
-		"id": "consumer1-8db1b117-61c6-4f91-867d-20ccd1ad8b3d"
-	}, {
-		"subscription": [
-			"RebalanceTopic",
-			"RebalanceTest"
-		],
-		"version": 0,
-		"id": "consumer3-bf2d11f4-1c73-4a39-b498-cfe76eb65bea"
-	}, {
-		"subscription": [
-			"RebalanceTopic",
-			"RebalanceTest"
-		],
-		"version": 0,
-		"id": "consumer2-9781058e-fad4-40e8-a69c-69afbae05184"
-	}
-];
+    "subscription": [
+        "RebalanceTopic",
+        "RebalanceTest"
+    ],
+    "version": 0,
+    "id": "consumer1-8db1b117-61c6-4f91-867d-20ccd1ad8b3d"
+}, {
+    "subscription": [
+        "RebalanceTopic",
+        "RebalanceTest"
+    ],
+    "version": 0,
+    "id": "consumer3-bf2d11f4-1c73-4a39-b498-cfe76eb65bea"
+}, {
+    "subscription": [
+        "RebalanceTopic",
+        "RebalanceTest"
+    ],
+    "version": 0,
+    "id": "consumer2-9781058e-fad4-40e8-a69c-69afbae05184"
+}];
 
 var consumerGroup = new ConsumerGroup(options, 'topic1');
 
-consumerGroup.assign(topicPartition, groupMembers, function (err, result) {
+consumerGroup.assign(topicPartition, groupMembers, function(err, result) {
 
-	console.log(result);
+    console.log(result);
 
 });
 
--- routes/consumerGroup.js
+++ routes/consumerGroup.js
@@ -1,34 +1,32 @@
 /*In an effort to make the API simpler you no longer need to create a client this is done inside the ConsumerGroup
 consumer ID do not need to be defined. There's a new ID to represent consumers called member ID and this is assigned to consumer after joining the group
 */
-// var express = require('express');
+var express = require('express');
 var kafka = require('kafka-node');
 var ConsumerGroup = kafka.ConsumerGroup;
-// var router = express.Router();
-router.get('/', function (req, res, next) {
-	var options = {
-		host: 'zookeeper:2181',
-		groupId: 'test-consumer-group',
-		protocol: [ 'test', 0, {}, function(){console.log(arguments} ],
-		migrateHLC: false,
-		migrateRolling: true
-	};
-	
-	// var consumerGroup = new ConsumerGroup(options, ['RebalanceTopic', 'RebalanceTest']);
-	var consumerGroup = new ConsumerGroup(options, 'topic1');
-	
-	
-	consumerGroup.on('message', function (message) {
-	console.log(message);
-});
-	
-	
-	
-	
-	
+var router = express.Router();
+router.get('/', function(req, res, next) {
+    var options = {
+        host: 'zookeeper:2181',
+        groupId: 'test-consumer-group',
+        protocol: ['test', 0, {}, function() {
+            console.log(arguments)
+        }],
+        migrateHLC: false,
+        migrateRolling: true
+    };
+
+    // var consumerGroup = new ConsumerGroup(options, ['RebalanceTopic', 'RebalanceTest']);
+    var consumerGroup = new ConsumerGroup(options, 'topic1');
+
+
+    consumerGroup.on('message', function(message) {
+        console.log(message);
+    });
+
+
 
 });
-
 
 
 
@@ -36,11 +34,4 @@ router.get('/', function (req, res, next) {
 
 
 
-
-
-
-
-
-
-
-// module.exports=router;
+module.exports = router;
